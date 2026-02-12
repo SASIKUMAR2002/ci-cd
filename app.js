dd02386c96d1f6b66f36548fa8574cb496dce8b6
root <root@ip-172-31-3-229.ap-south-1.compute.internal>
2026-02-12 06:36:03 +0000

    test4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Deployment test Successful 🚀");
+  res.send("CI/CD Deployment testi 1 Successful 🚀");
 });
 
 app.listen(3000, () => console.log("Server running"));
